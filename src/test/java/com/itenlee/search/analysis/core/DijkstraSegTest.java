3a67a7156b5a96bb6301e183b8a813d11c2560a9
tenlee <tenlee2012@users.noreply.github.com>
2022-04-08 23:44:42 +0800

    Release (#40)
    * 支持字母+数字不拆开分词
    
    * 自定义词库做归一化处理
    
    * v7.11.1
    
    * v7.11.2
    
    * v7.12.0
    
    * v7.12.1
    
    * v7.13.0
    
    * v7.13.1
    
    * v7.13.2
    
    * v7.13.3
    
    * v7.13.4
    
    * update for 7.14.0
    
    * Create main.yml
    
    * v7.14.1
    
    * v7.0.0
    
    * v7.0.0
    
    * auto v7.0.1
    
    * auto v7.1.0
    
    * auto v7.1.1
    
    * auto v7.2.0
    
    * auto v7.2.1
    
    * auto v7.3.0
    
    * auto v7.3.1
    
    * auto v7.3.2
    
    * auto v7.4.0
    
    * auto v7.4.1
    
    * auto v7.4.2
    
    * auto v7.5.0
    
    * auto v7.5.1
    
    * auto v7.5.2
    
    * auto v7.6.0
    
    * auto v7.6.1
    
    * auto v7.6.2
    
    * auto v7.7.0
    
    * auto v7.7.1
    
    * auto v7.8.0
    
    * auto v7.8.1
    
    * auto v7.9.0
    
    * auto v7.9.1
    
    * auto v7.9.2
    
    * auto v7.9.3
    
    * auto v7.10.0
    
    * auto v7.10.1
    
    * auto v7.10.2
    
    * auto v7.11.1
    
    * auto v7.11.2
    
    * auto v7.12.0
    
    * auto v7.12.1
    
    * auto v7.13.0
    
    * auto v7.13.1
    
    * auto v7.13.2
    
    * auto v7.13.3
    
    * auto v7.13.4
    
    * auto v7.14.0
    
    * auto v7.14.1
    
    * v7.14.2
    
    * v7.15.0
    
    * v7.15.1
    
    * v7.15.2
    
    * 增加分词器介绍
    
    * 7.16.0
    
    * v7.0.0 for log4j-2.15.0
    
    * 支持多个扩展词库，支持长度<=N自动分词
    
    * 支持自动分词
    
    * add 7.16.2
    
    * 代码优化
    
    * auto v7.0.1
    
    * auto v7.1.0
    
    * auto v7.1.1
    
    * auto v7.2.0
    
    * auto v7.2.1
    
    * auto v7.3.0
    
    * auto v7.3.1
    
    * auto v7.3.2
    
    * auto v7.4.0
    
    * auto v7.4.1
    
    * auto v7.4.2
    
    * auto v7.5.0
    
    * auto v7.5.1
    
    * auto v7.5.2
    
    * auto v7.6.0
    
    * auto v7.6.1
    
    * auto v7.6.2
    
    * auto v7.7.0
    
    * auto v7.7.1
    
    * auto v7.8.0
    
    * auto v7.8.1
    
    * auto v7.9.0
    
    * auto v7.9.1
    
    * auto v7.9.2
    
    * auto v7.9.3
    
    * auto v7.10.0
    
    * auto v7.10.1
    
    * auto v7.10.2
    
    * auto v7.11.1
    
    * auto v7.11.2
    
    * auto v7.12.0
    
    * auto v7.12.1
    
    * auto v7.13.0
    
    * auto v7.13.1
    
    * auto v7.13.2
    
    * auto v7.13.3
    
    * auto v7.13.4
    
    * auto v7.14.0
    
    * auto v7.14.1
    
    * auto v7.14.2
    
    * auto v7.15.0
    
    * auto v7.15.1
    
    * auto v7.15.2
    
    * auto v7.16.0
    
    * auto v7.16.2
    
    * auto v7.16.3
    
    * auto v7.17.0
    
    * auto v7.17.1
    
    * auto v7.17.2
    
    * auto v8.0.0
    
    * auto v8.0.1
    
    * auto v8.1.0
    
    * auto v8.1.1
    
    * auto v8.1.2
    
    Co-authored-by: tenlee2012 <tenlee2012@163.com>

--- src/test/java/com/itenlee/search/analysis/core/DijkstraSegTest.java
+++ src/test/java/com/itenlee/search/analysis/core/DijkstraSegTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class DijkstraSegTest extends TestCase {
 
     Settings settings = Settings.builder().put("path.home", "~/workspace/elasticsearch-analysis-hao/target")
-        .put("enableFallBack", "true").put("enableFailDingMsg", "false").build();
+            .put("enableFallBack", "true").put("enableFailDingMsg", "false").build();
     Environment env = new Environment(settings, null);
     Configuration configuration = new Configuration(null, null, env, settings).setIndexMode(true);
 
@@ -32,5 +32,24 @@ public class DijkstraSegTest extends TestCase {
         for (Term term : terms) {
             System.out.printf("start=%d, end=%d, %s\n", term.getOffset(), term.getEnd(), term.getText());
         }
+    }
+
+    @Test
+    public void testPerformance() {
+        Dictionary.initial(configuration);
+        Dictionary dic = Dictionary.getInstance();
+        String s = "俄罗斯大厦门🤣";
+        System.out.println("====start===");
+        s = "俄罗斯 孙成功 GPS卫星定位,中华字诸葛明,中華字,徐骁snh48,99mm,66ka,66,demo,99.990.1,9.9 徐庆年 ";
+        for (int i = 0; i < 10; i++) {
+            s += s;
+        }
+        DijkstraSeg dijkstraSeg = new DijkstraSeg(dic);
+        long t1 = System.nanoTime();
+        for (int i = 0; i < 100; i++) {
+            dijkstraSeg.segSentence(s, true, 3, false);
+        }
+        long t2 = System.nanoTime();
+        System.out.println("cost " + (t2 - t1) / 1000 / 100 + " microseconds");
     }
 }
